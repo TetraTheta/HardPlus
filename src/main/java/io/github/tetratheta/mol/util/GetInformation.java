76f5bc1da9304350cec92410a6fe1b5d6ee4222f
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2026-01-18 14:29:27 +0900

    Modernize JavaDoc

--- src/main/java/io/github/tetratheta/mol/util/GetInformation.java
+++ src/main/java/io/github/tetratheta/mol/util/GetInformation.java
@@ -7,27 +7,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * Provides information about server and this library
- */
+/// Provides information about server and this library
 @SuppressWarnings("unused")
 public final class GetInformation {
-  /**
-   * Returns current Bukkit API version. This would be something like {@code 1.20.1-R0.1-SNAPSHOT}.
-   *
-   * @return Current Bukkit API version
-   * @deprecated For {@code 1.20.1-R0.1-SNAPSHOT}-like result, Use {@code Bukkit.getServer().getBukkitVersion()}. Use {@code Bukkit.getServer().getMinecraftVersion()} for {@code 1.20.1}-like result.
-   */
+  /// Returns current Bukkit API version. This would be something like `1.20.1-R0.1-SNAPSHOT`.
+  ///
+  /// @return Current Bukkit API version
+  /// @deprecated For `1.20.1-R0.1-SNAPSHOT`-like result, Use `Bukkit.getServer().getBukkitVersion()`. Use `Bukkit.getServer().getMinecraftVersion()` for `1.20.1`-like result.
   @Deprecated
   public static @NotNull String serverVersion() {
     return Bukkit.getServer().getBukkitVersion();
   }
 
-  /**
-   * Returns base package name of this library. If you don't relocate this library, it would return {@code com.github.tetratheta.mol}.
-   *
-   * @return Package name of this library
-   */
+  /// Returns base package name of this library. If you don't relocate this library, it would return `com.github.tetratheta.mol`.
+  ///
+  /// @return Package name of this library
   public static @NotNull String libraryPackage() {
     // This should return 'com.github.tetratheta.mol'
     String currentPackageName = GetInformation.class.getName();
--- src/main/java/io/github/tetratheta/mol/util/Task.java
+++ src/main/java/io/github/tetratheta/mol/util/Task.java
@@ -2,13 +2,9 @@ package io.github.tetratheta.mol.util;
 
 import org.bukkit.scheduler.BukkitRunnable;
 
-/**
- * Helper interface for class that creates {@code BukkitRunnable} which produces {@code BukkitTask}.
- */
+/// Helper interface for class that creates `BukkitRunnable` which produces `BukkitTask`.
 @SuppressWarnings("unused")
 public interface Task {
-  /**
-   * @return Returns {@code BukkitRunnable} which can return {@code BukkitTask} with {@code BukkitRunnable#getTask()}
-   */
+  /// @return Returns `BukkitRunnable` which can return `BukkitTask` with `BukkitRunnable#getTask()`
   BukkitRunnable getTask();
 }
